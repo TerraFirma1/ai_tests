00e988c3149666eeb60618bcbed237d61d3b26a4
TerraFirma1 <edward.royce@smartcontract.com>
2023-06-18 15:30:04 +0100

    initial commit

--- test.py
+++ test.py
@@ -1,1 +1,14 @@
 import json
+import docx2txt
+
+
+def read_file(name, path):
+    res = docx2txt.process(name, path)
+    return res
+
+
+content = read_file('Edward_Royce_v2.docx', './')
+
+print(content)
+
+
